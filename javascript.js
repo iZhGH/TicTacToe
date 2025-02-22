1361e3fb8397b90c57ce5e7c26db7ab91b12f6a0
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-02-22 10:00:08 -0800

    feat(javascript.js): Start module pattern - Create Gameboard object and anonymous function - Create gameboard array itself - Create initBoard,addPiece,resetBoard as functions - Return each function making them publicly available

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 const Gameboard = (function() {
-    let gameBoard = [[1,2,3], [1,2,3],[1,2,3]];
+    let gameBoard = [];
+    const initializeBoard = function () {
+        gameBoard = [1,2,3], [1,2,3],[1,2,3];
+        return
+    }
 
+
+    return {initializeBoard }
 })();
