f59fda0142e7657e755b7258587e204ea6826503
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-02-22 10:00:49 -0800

    fix(javascript.js): Return functions - Return all 3 Gameboard functions to be publicly available

--- javascript.js
+++ javascript.js
@@ -1,10 +1,25 @@
 const Gameboard = (function() {
     let gameBoard = [];
-    const initializeBoard = function () {
-        gameBoard = [1,2,3], [1,2,3],[1,2,3];
+
+    const initBoard = function () {
+        gameBoard = [[1,2,3], [4,5,6],[7,8,9]];
+        console.log(gameBoard)
+        return
+    }
+
+    const addPiece = function (piece, position) {
+
+        return
+    }
+
+    const resetBoard = function () {
+
         return
     }
 
 
-    return {initializeBoard }
+    
+    return { initBoard, addPiece, resetBoard}
 })();
+
+Gameboard.initBoard();
