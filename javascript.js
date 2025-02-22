ce1cbfbf3c94c2d6b1c796ae99d3c75c65f0519c
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-02-22 10:02:39 -0800

    refactor(javascript.js): Unnecessary return statements - Remove return statements from publicly available methods in Gameboard object's anonymous function

--- javascript.js
+++ javascript.js
@@ -4,17 +4,17 @@ const Gameboard = (function() {
     const initBoard = function () {
         gameBoard = [[1,2,3], [4,5,6],[7,8,9]];
         console.log(gameBoard)
-        return
+        
     }
 
     const addPiece = function (piece, position) {
 
-        return
+        
     }
 
     const resetBoard = function () {
 
-        return
+        
     }
 
 
