d1e2806714d9200eb0dc94cc2b4697afbe9ce7b6
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-02-23 22:20:26 -0800

    feat(javascript.js): Prevent overwritten spot - Remove ability for player to replace another users piece with their own

--- javascript.js
+++ javascript.js
@@ -58,67 +58,33 @@ const Gameboard = (function () {
     let winnerDetermined = false;
 
     const playGame = function () {
-      
-      
-      for(let i =0; i < maxTurns; i++) {
-        // UI BASED
         messageBoard.textContent = `It's Player ${userTurn}'s Turn!`;
         gridPosition.forEach((position) => position.addEventListener("click", () => {
-            let spotChosen = event.target;
-            Gameboard.addPiece(spotChosen,userTurn);
-            GameController.checkWinner(userTurn);
-            if (winnerDetermined == true) {
-            console.log(`Congratulations, Player ${userTurn} has won!`);
-            i = 9;
+            if ((event.target.textContent !== "X") && (event.target.textContent !== "O")) {
+                let spotChosen = event.target;
+                Gameboard.addPiece(spotChosen,userTurn);
+                GameController.checkWinner(userTurn);
+                if (winnerDetermined == true) {
+                console.log(`Congratulations, Player ${userTurn} has won!`);
+                i = 9;
+                }
+                (userTurn == 1) ? userTurn = 2 : userTurn = 1;
+                messageBoard.textContent = `It's Player ${userTurn}'s Turn!`;
+
             }
-            (userTurn == 1) ? userTurn = 2 : userTurn = 1;
+         else {
+            messageBoard.textContent = "Please select an empty spot!";
+         }
+        
 
         }));
-
-        // CONSOLE-BASED
-      userPosition = prompt("Enter a number to place your X/O on that corresponding position.") // TODO: Check if valid number
-      userPosition = Number(userPosition);
-      
-      Gameboard.addPiece(userPosition, userTurn);
-      Gameboard.displayBoard();
-      GameController.checkWinner(userTurn);
-      if (winnerDetermined == true) {
-        console.log(`Congratulations, Player ${userTurn} has won!`);
-        i = 9;
-    }
-      (userTurn == 1) ? userTurn = 2 : userTurn = 1;
-      }
-
     };
     
     const checkWinner = function (userTurn) {
 
-        for (let row = 0; row <3; row++ ) { // Check rows for win condition
-            if(checkMatch(Gameboard.getBoardPosition(row,0),Gameboard.getBoardPosition(row,1),Gameboard.getBoardPosition(row,2),userTurn)) {
-                winnerDetermined = true;
-            }
-        }
 
-        for (let column = 0; column <3; column++ ) { // Check columns for win condition
-            if(checkMatch(Gameboard.getBoardPosition(0,column),Gameboard.getBoardPosition(1,column),Gameboard.getBoardPosition(2,column),userTurn)) {
-                winnerDetermined = true;
-            }
-        }
 
-        for (let row = 0; row < 1; row++ ) { // Check left-right diagonal for win condition
-            if(checkMatch(Gameboard.getBoardPosition(0,0),Gameboard.getBoardPosition(1,1),Gameboard.getBoardPosition(2,2),userTurn)) {
-                winnerDetermined = true;
-                row++
-            }
-        }
-
-        for (let row = 0; row < 1; row++ ) { // Check right-left diagonal for win condition
-            if(checkMatch(Gameboard.getBoardPosition(0,2),Gameboard.getBoardPosition(1,1),Gameboard.getBoardPosition(2,0),userTurn)) {
-                winnerDetermined = true;
-                row++
-            }
-        }
-
+       
         
     }
 
